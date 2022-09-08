f7b93fe5bad00bbd51d150c979fb0adf867ede00
Huy Tran <huy.b.tran@capgemini.com>
2022-09-08 17:57:56 +0700

    update validation addUser and Register

--- src/pages/Users/UserDetail.js
+++ src/pages/Users/UserDetail.js
@@ -7,10 +7,13 @@ import { styled } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 
+import { ErrorMessage, useFormik, Formik, Form, Field } from "formik";
+
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getIdBlog } from "../../redux/Users/UsersSlice";
 import { getUserById } from "../../redux/Users/UsersSlice";
+import { Schema } from "../../services/Schema";
 
 export default function UserDetail() {
   const { id } = useParams();
@@ -26,135 +29,184 @@ export default function UserDetail() {
     dispatch(getIdBlog(id));
   }, []);
 
+  const initialValues = {
+    firstName: userById?.firstName,
+    lastName: userById?.lastName,
+    email: userById?.email,
+    password: userById?.password,
+    role: userById?.role,
+  };
+
+  console.log("initialValues", initialValues);
+
+  const handleSubmit = (values) => {
+    console.log("values when submit", values);
+  };
+
   return (
     <div
       className={`container rounded ${
         mode === "light" ? "bg-white" : "bg-dark"
       }`}
     >
-      <div className="row">
-        <div className="d-flex justify-content-center align-items-center col-md-3 border-right">
-          <div className="d-flex flex-column align-items-center text-center p-3 py-5">
-            <Avatar
-              alt="Remy Sharp"
-              src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
-              sx={{ width: 150, height: 150, mb: 3 }}
-            />
+      <Formik
+        initialValues={initialValues}
+        validationSchema={Schema}
+        onSubmit={handleSubmit}
+      >
+        {(formikProps) => {
+          const { values, error, touched } = formikProps;
 
-            <span className="font-weight-bold">
-              {userById.firstName} {userById.lastName}
-            </span>
-            <span
-              className={`${
-                mode === "light" ? "text-black-50" : "text-white-50"
-              }`}
-            >
-              {userById.email}
-            </span>
-            <span
-              className={`${
-                mode === "light" ? "text-black-50" : "text-white-50"
-              }`}
-            >
-              ID: {userById.userID}
-            </span>
-            <span> </span>
-          </div>
-        </div>
-        <div className="col-md-5 border-right">
-          <div className="p-3 py-5">
-            <div className="d-flex justify-content-between align-items-center mb-3">
-              <h4 className="text-right">Profile</h4>
-            </div>
-            <div className="row mt-2">
-              <div className="col-md-6">
-                <label className="labels">First Name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="first name"
-                  value={userById.firstName}
-                />
+          return (
+            <Form onSubmit={formikProps.handleSubmit}>
+              <div className="row">
+                <div className="d-flex justify-content-center align-items-center col-md-3 border-right">
+                  <div className="d-flex flex-column align-items-center text-center p-3 py-5">
+                    <Avatar
+                      alt="Remy Sharp"
+                      src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
+                      sx={{ width: 150, height: 150, mb: 3 }}
+                    />
+
+                    <span className="font-weight-bold">
+                      {userById.firstName} {userById.lastName}
+                    </span>
+                    <span
+                      className={`${
+                        mode === "light" ? "text-black-50" : "text-white-50"
+                      }`}
+                    >
+                      {userById.email}
+                    </span>
+                    <span
+                      className={`${
+                        mode === "light" ? "text-black-50" : "text-white-50"
+                      }`}
+                    >
+                      ID: {userById.userID}
+                    </span>
+                    <span> </span>
+                  </div>
+                </div>
+                <div className="col-md-5 border-right">
+                  <div className="p-3 py-5">
+                    <div className="d-flex justify-content-between align-items-center mb-3">
+                      <h4 className="text-right">Profile</h4>
+                    </div>
+                    <div className="row mt-2">
+                      <div className="col-md-6">
+                        <label className="labels">First Name</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="first name"
+                          value={formikProps.values.firstName}
+                          name="firstName"
+                          id="firstName"
+                          onChange={formikProps.handleChange}
+                          onBlur={formikProps.handleBlur}
+                        />
+                      </div>
+                      <div className="col-md-6">
+                        <label className="labels">Last Name</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="last name"
+                          value={formikProps.values.lastName}
+                          name="lastName"
+                          id="lastName"
+                          onChange={formikProps.handleChange}
+                          onBlur={formikProps.handleBlue}
+                        />
+                      </div>
+                    </div>
+                    <div className="row mt-3">
+                      <div className="col-md-12">
+                        <label className="labels">Email</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="email"
+                          name="email"
+                          id="email"
+                          value={formikProps.values.email}
+                          onChange={formikProps.handleChange}
+                          onBlur={formikProps.handleBlur}
+                        />
+                      </div>
+                    </div>
+                    <div className="row mt-3">
+                      <div className="col-md-12">
+                        <label className="labels">Password</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="password"
+                          name="password"
+                          id="password"
+                          value={formikProps.values.password}
+                          onChange={formikProps.handleChange}
+                          onBlur={formikProps.handleBlur}
+                        />
+                      </div>
+                    </div>
+                    <div className="row mt-3">
+                      <div className="col-md-12">
+                        <label className="labels">Role</label>
+                        <input
+                          disabled
+                          type="text"
+                          className="form-control"
+                          placeholder="role"
+                          name="role"
+                          id="role"
+                          value={formikProps.values.role}
+                        />
+                      </div>
+                    </div>
+                    <div className="mt-5 text-center">
+                      <button
+                        className="btn btn-primary profile-button"
+                        type="submit"
+                      >
+                        Save Profile
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-4">
+                  <div className="p-3 py-5">
+                    <div className="d-flex justify-content-between align-items-center experience">
+                      <h4>Projects</h4>
+                    </div>
+                    <br />
+                    <div className="col-md-12">
+                      <label className="labels">Experience in Designing</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="experience"
+                        defaultValue
+                      />
+                    </div>{" "}
+                    <br />
+                    <div className="col-md-12">
+                      <label className="labels">Additional Details</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="additional details"
+                        defaultValue
+                      />
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="col-md-6">
-                <label className="labels">Last Name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="last name"
-                  value={userById.lastName}
-                />
-              </div>
-            </div>
-            <div className="row mt-3">
-              <div className="col-md-12">
-                <label className="labels">Email</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="enter phone number"
-                  value={userById.email}
-                />
-              </div>
-            </div>
-            <div className="row mt-3">
-              <div className="col-md-12">
-                <label className="labels">Password</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="enter phone number"
-                  value={userById.password}
-                />
-              </div>
-            </div>
-            <div className="row mt-3">
-              <div className="col-md-12">
-                <label className="labels">Role</label>
-                <input
-                  disabled
-                  type="text"
-                  className="form-control"
-                  placeholder="enter phone number"
-                  value={userById.role}
-                />
-              </div>
-            </div>
-            <div className="mt-5 text-center">
-              <button className="btn btn-primary profile-button" type="button">
-                Save Profile
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="p-3 py-5">
-            <div className="d-flex justify-content-between align-items-center experience">
-              <h4>Projects</h4>
-            </div>
-            <br />
-            <div className="col-md-12">
-              <label className="labels">Experience in Designing</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="experience"
-                defaultValue
-              />
-            </div>{" "}
-            <br />
-            <div className="col-md-12">
-              <label className="labels">Additional Details</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="additional details"
-                defaultValue
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+            </Form>
+          );
+        }}
+      </Formik>
     </div>
     // <Box>
     //   <Box sx={{ height: 150, backgroundColor: "#d6d6d6", borderRadius: 5 }}>
